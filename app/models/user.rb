a8a7f1a4ef523b60cbbb3d2cd4b123f0180a2547
Muhammad Habib Ali <m.habibali@hotmail.com>
2020-12-09 21:12:21 +0500

    removed extra code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :rememberable, :validatable
   
   has_many :blood_gulcose_readings, dependent: :destroy
   after_create :assign_default_role
